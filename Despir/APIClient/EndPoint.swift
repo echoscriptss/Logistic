cb983291dff0d34c4d34392b1c96a9b0471bbf74
Prabhjoot Singh <prabhjoot.singh@mobileprogramming.com>
2026-01-21 19:06:06 +0530

    Worked on implementation of get filters api

--- Despir/APIClient/EndPoint.swift
+++ Despir/APIClient/EndPoint.swift
@@ -14,11 +14,10 @@ enum EndPoint: Equatable {
     case forgotPassword
     case verifyOTP
     case resetPassword
-    case getRoles // Temp for get par in httpmethod
     case changePassword
     case updateProfile
     case getOrders(page: Int, limit: Int,  isDelivered: Bool, assignedToMe: Bool, sortOrder: String, orderBy: String, customer: String, carrier: String, enteredBy: String, dispatcher: String, pickupStart: String, pickupEnd: String, deliveryStart: String, deliveryEnd: String, orderType: String, scheduleEarlyDate: String, tsa: Bool)
-
+    case getFilterOptions
     
     var endpoint: String {
         switch self {
@@ -32,8 +31,6 @@ enum EndPoint: Equatable {
             return "/user/forgot_password"
         case .verifyOTP:
             return "/user/validate_pw_otp"
-        case .getRoles:
-            return "/user/all_roles"
         case .changePassword:
             return "/user/change_password"
         case .resetPassword:
@@ -42,7 +39,8 @@ enum EndPoint: Equatable {
           return "/user/update/\(DataManager.userUuid ?? "")"
         case .getOrders(page: let page, limit: let limit, isDelivered: let is_delivered, assignedToMe: let assigned_to_me, sortOrder: let sortOrder, orderBy: let orderby, customer: let customer, carrier: let carrier, enteredBy: let enteredby, dispatcher: let dispatcher, pickupStart: let pickup_start, pickupEnd: let pickup_end, deliveryStart: let delivery_start, deliveryEnd: let delivery_end, orderType: let ordertype, scheduleEarlyDate: let schedule_early_date, tsa: let tsa):
           return "/orders/get_orders?page=\(page)&limit=\(limit)&is_delivered=\(is_delivered)&assigned_to_me=\(assigned_to_me)&sort_order=\(sortOrder)&orderby=\(orderby)&customer=\(customer)&carrier=\(carrier)&enteredby=\(enteredby)&dispatcher=\(dispatcher)&pickup_start=\(pickup_start)&pickup_end=\(pickup_end)&delivery_start=\(delivery_start)&delivery_end=\(delivery_end)&ordertype=\(ordertype)&schedule_early_date=\(schedule_early_date)&tsa=\(tsa)"
-          
+        case .getFilterOptions:
+          return "/orders/get_filters_options"
         }
     }
     
@@ -50,7 +48,7 @@ enum EndPoint: Equatable {
         switch self {
         case .login, .verifyMfa, .resendOtpMfa, .forgotPassword, .changePassword , .verifyOTP, .resetPassword, .updateProfile:
             return .POST
-        case .getRoles, .getOrders(page: _, limit: _, isDelivered: _, assignedToMe: _, sortOrder: _, orderBy: _, customer: _, carrier: _,enteredBy: _, dispatcher: _, pickupStart: _, pickupEnd: _, deliveryStart: _, deliveryEnd: _ ,orderType: _, scheduleEarlyDate: _, tsa: _):
+        case .getOrders(page: _, limit: _, isDelivered: _, assignedToMe: _, sortOrder: _, orderBy: _, customer: _, carrier: _,enteredBy: _, dispatcher: _, pickupStart: _, pickupEnd: _, deliveryStart: _, deliveryEnd: _ ,orderType: _, scheduleEarlyDate: _, tsa: _), .getFilterOptions:
             return .GET
         }
     }
